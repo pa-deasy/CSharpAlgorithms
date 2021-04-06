92b291fa79ff7d7c745f29e80bf8038eecb39641
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-04-05 17:56:57 -0700

    Add selection sort example

---
+++ SelectionSortArrayExample/ArraySortOperations.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace SelectionSortArrayExample
+{
+    public static class ArraySortOperations
+    {
+        public static int[] SelectionSort(this int[] numbersArray)
+        {
+            var orderedArray = new int[0];
+
+            while(numbersArray.Length > 0)
+            {
+                var smallestIndex = numbersArray.FindSmallestIndex();
+
+                orderedArray = orderedArray.Append(numbersArray[smallestIndex]).ToArray();
+
+                numbersArray = numbersArray.RemoveAt(smallestIndex);
+            }
+
+            return orderedArray;
+        }
+
+        private static int FindSmallestIndex(this int[] numbersArray)
+        {
+            var smallestIndex = 0;
+
+            for(int i = 1; i < numbersArray.Length; i++ )
+            {
+                if (numbersArray[i] < numbersArray[smallestIndex])
+                    smallestIndex = i;
+            }
+
+            return smallestIndex;
+        }
+
+        private static int[] RemoveAt(this int[] numbersArray, int index)
+        {
+            var numbersList = numbersArray.ToList();
+            numbersList.RemoveAt(index);
+            return numbersList.ToArray();
+        }
+    }
+}
