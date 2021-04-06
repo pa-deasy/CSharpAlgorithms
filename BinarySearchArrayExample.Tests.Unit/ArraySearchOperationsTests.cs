c8424660a8e6255b59b20280a4808fa0970ee32d
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-04-05 17:07:07 -0700

    Add arraysearch example

---
+++ BinarySearchArrayExample.Tests.Unit/ArraySearchOperationsTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace BinarySearchArrayExample.Tests.Unit
+{
+    [TestFixture]
+    public class ArraySearchOperationsTests
+    {
+        private int[] _numbersArray = new int[] { 1, 3, 5, 7, 9 };
+
+        [TestCase(1)]
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(7)]
+        [TestCase(9)]
+        public void Given_IntArray_When_Exists_Returned_Element(int target)
+        {
+            _numbersArray.BinarySearch(target).Should().BeTrue();
+        }
+
+        [Test]
+        public void Given_IntArray_When_DoesNotExist_Returned_Null()
+        {
+            _numbersArray.BinarySearch(2).Should().BeFalse();
+        }
+    }
+}
---
+++ BinarySearchArrayExample/ArraySearchOperations.cs
@@ -0,0 +1,28 @@
+﻿namespace BinarySearchArrayExample
+{
+    public static class ArraySearchOperations
+    {
+        public static bool BinarySearch(this int[] inputArray, int target)
+            => inputArray.BinarySearch(target, 0, inputArray.Length - 1);
+
+        private static bool BinarySearch(this int[] inputArray, int target, int low, int high)
+        {
+            if (low > high)
+                return false;
+
+            var mid = (low + high) / 2;
+            var guess = inputArray[mid];
+
+            if (guess == target)
+                return true;
+
+            if (target > guess)
+                return inputArray.BinarySearch(target, mid + 1, high);
+
+            if (guess > target)
+                return inputArray.BinarySearch(target, low, mid - 1);
+
+            return false;
+        }
+    }
+}
