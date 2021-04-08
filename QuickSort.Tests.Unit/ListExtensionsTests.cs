f19a9efdd5a6fef0288241ce149beb54e7d3ef79
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-04-07 20:34:00 -0700

    quick sort numbers example

---
+++ QuickSort.Tests.Unit/ListExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace QuickSort.Tests.Unit
+{
+    [TestFixture]
+    public class ListExtensionsTests
+    {
+        [Test]
+        public void Given_ListNumbers_When_QuickSorted_Then_ReturnsSortedList()
+        {
+            var numbers = new List<int> { 10, 5, 2, 3, 5 };
+            var sortedNumbers = numbers.QuickSort();
+
+            sortedNumbers[0].Should().Be(2);
+            sortedNumbers[1].Should().Be(3);
+            sortedNumbers[2].Should().Be(5);
+            sortedNumbers[3].Should().Be(5);
+            sortedNumbers[4].Should().Be(10);
+        }
+    }
+}
