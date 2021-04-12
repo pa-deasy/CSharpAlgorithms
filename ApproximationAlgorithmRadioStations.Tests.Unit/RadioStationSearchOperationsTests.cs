220277667744c47ff5c7a5518d492bf86c6d321c
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-04-11 18:44:25 -0700

    Approximation example with sets

---
+++ ApproximationAlgorithmRadioStations.Tests.Unit/RadioStationSearchOperationsTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApproximationAlgorithmRadioStations.Tests.Unit
+{
+    [TestFixture]
+    public class RadioStationSearchOperationsTests
+    {
+        [Test]
+        public void Given_RadioStations_When_SearchedForRequiredStates_Then_ReturnsNeededStations()
+        {
+            var requiredStates = new HashSet<string> { "mt", "wa", "or", "id", "nv", "ut", "ca", "az" };
+            var minimumStations = Stations.MimimumToReach(requiredStates);
+
+            Assert.AreEqual(4, minimumStations.Count);
+            Assert.Contains("kone", minimumStations.ToList());
+            Assert.Contains("ktwo", minimumStations.ToList());
+            Assert.Contains("kthree", minimumStations.ToList());
+            Assert.Contains("kfive", minimumStations.ToList());
+        }
+
+        private Dictionary<string, HashSet<string>> Stations =>
+            new Dictionary<string, HashSet<string>> 
+            {
+                { "kone", new HashSet<string>{ "id", "nv", "ut" } },
+                { "ktwo", new HashSet<string>{ "wa", "id", "mt" } },
+                { "kthree", new HashSet<string>{ "or", "nv", "ca" } },
+                { "kfour", new HashSet<string>{ "nv", "ut" } },
+                { "kfive", new HashSet<string>{ "ca", "az" } }
+            };
+    }
+}
---
+++ ApproximationAlgorithmRadioStations/RadioStationSearchOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApproximationAlgorithmRadioStations
+{
+    public static class RadioStationSearchOperations
+    {
+        public static HashSet<string> MimimumToReach(this Dictionary<string, HashSet<string>> radioStations, HashSet<string> requiredStates)
+        {
+            var requiredRadioStations = new HashSet<string>();
+
+            while (requiredStates.Any())
+            {
+                var bestNumberofMatches = radioStations.Max(r => r.Value.Intersect(requiredStates).Count());
+                var bestMatch = radioStations.First(r => r.Value.Intersect(requiredStates).Count() == bestNumberofMatches).Key;
+
+                requiredRadioStations.Add(bestMatch);
+                requiredStates.ExceptWith(radioStations[bestMatch]);
+            }
+
+            requiredRadioStations.ToList().ForEach(r => Console.WriteLine($"Radio station {r} is needed"));
+            return requiredRadioStations;
+        }
+    }
+}
