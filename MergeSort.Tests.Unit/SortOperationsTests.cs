61f71ddb00d85fce0d0c8e487c52027743aa95d9
LAPTOP-2MP57682\Pa-De <pa-deasy@hotmail.com>
2021-04-16 19:05:10 -0700

    Example of merge sorts, use of arrays may be more performant

---
+++ MergeSort.Tests.Unit/SortOperationsTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace MergeSort.Tests.Unit
+{
+    [TestFixture]
+    public class SortOperationsTests
+    {
+        [Test]
+        public void Given_UnsortedNumbers_When_Sorted_Then_ReturnsNumbersInOrder()
+        {
+            var numbers = new int[] { 9, 2, 55, 57, 15, 1 };
+            var expectedNumbers = new int[] { 1, 2, 9, 15, 55, 57 };
+
+            var sortedNumbers = numbers.SortNumbers();
+
+            Assert.AreEqual(expectedNumbers, sortedNumbers);
+        }
+    }
+}
---
+++ MergeSort/SortOperations.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeSort
+{
+    public static class SortOperations
+    {
+        public static int[] SortNumbers(this int[] numbers)
+        {
+            if (!numbers.Any() || numbers.Length == 1)
+                return numbers;
+
+            var numbersQueue = new Queue<int>(numbers);
+            var half = numbers.Length / 2;
+            var leftNumbers = new List<int>();
+            var rightNumbers = new List<int>();
+
+            while (numbersQueue.Any())
+            {
+                if (numbersQueue.Count <= half)
+                    leftNumbers.Add(numbersQueue.Dequeue());
+
+                else
+                    rightNumbers.Add(numbersQueue.Dequeue());
+            }
+
+            return leftNumbers.ToArray().SortNumbers()
+                .MergeWith(rightNumbers.ToArray().SortNumbers());
+        }
+
+        private static int[] MergeWith(this int[] leftNumbers, int[] rightNumbers)
+        {
+            var mergedNumbers = new List<int>();
+
+            var leftQueue = new Queue<int>(leftNumbers);
+            var rightQueue = new Queue<int>(rightNumbers);
+
+            while(leftQueue.Any())
+            {
+                if (!rightQueue.Any() || leftQueue.Peek() < rightQueue.Peek())
+                    mergedNumbers.Add(leftQueue.Dequeue());
+
+                else
+                    mergedNumbers.Add(rightQueue.Dequeue());
+            }
+
+            while(rightQueue.Any())
+            {
+                var popped = rightQueue.Dequeue();
+
+                if (!rightQueue.Any() || popped < rightQueue.Peek())
+                    mergedNumbers.Add(popped);
+
+                else
+                {
+                    mergedNumbers.Add(rightQueue.Dequeue());
+                    rightQueue.Enqueue(popped);
+                }
+            }
+
+            return mergedNumbers.ToArray();
+        }
+    }
+}
